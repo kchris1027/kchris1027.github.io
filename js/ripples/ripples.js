72dbc2c2b1a40523895976dd5191db74f63ecfff
kchris1027 <127928457+kchris1027@users.noreply.github.com>
2023-04-15 05:16:35 +0000

    Deploying to master from  @ 64937c2d79ef016dfa805ec71472dc51bda54eb1 🚀

--- js/ripples/ripples.js
+++ js/ripples/ripples.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
     try {
       $("#page-header").ripples({
-        resolution: 512,
+        resolution: 1024,
         dropRadius: 10, //px
         perturbance: 0.04,
       });
     } catch (e) {
       $(".error").show().text(e);
     }
+    // $(window).resize(function() {
+    //   // 在这里添加适配网页缩放的代码
+    //   // 比如重新计算水波纹效果的参数等
+    //   // 例如：
+    //   $("#page-header").ripples("updateSize");
+    // });
   });
