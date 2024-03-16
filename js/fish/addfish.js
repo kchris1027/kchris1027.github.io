21385825bd47f13f3b5a5eae60a3ac4d2e15afe6
kchris1027 <127928457+kchris1027@users.noreply.github.com>
2024-03-16 16:59:35 +0000

    Deploying to master from @ kchris1027/kchris1027.github.io@580a1bbc70692445643db8f2879428e5ac57f929 🚀

--- js/fish/addfish.js
+++ js/fish/addfish.js
@@ -34,4 +34,4 @@ function fish() {
       }),       
       this
     );
-  }
+  }<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
--- js/ripples/jquery.ripples.js
+++ js/ripples/jquery.ripples.js
@@ -900,4 +900,4 @@ $.fn.ripples.noConflict = function() {
 	return this;
 };
 
-})));
+})));<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
